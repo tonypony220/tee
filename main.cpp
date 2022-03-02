12f426a6da6e0c17bac3efd87049c598cc63946a
Marq Ehtel <mehtel@il-e5.msk.21-school.ru>
2022-03-02 17:59:44 +0300

    d

--- main.cpp
+++ main.cpp
@@ -16,38 +16,19 @@ bool open_file(const std::string& name,
 	return true;
 }
 
-int main(int argc, const char** argv)
-{
+int main(int argc, const char** argv) {
 	std::vector<std::shared_ptr<std::ofstream> > streams;
-//	teebuf::ptrs bufs;
-//	std::vector< std::basic_streambuf<char> * > bufs;
-
-			//    std::ofstream log( argv[1] );
    	std::string a(argv[1]);
    	bool append = a == "-a";
    	int i = 0;
    	append && i++;
    	while (++i < argc) {
 		std::shared_ptr<std::ofstream> out(new std::ofstream);
-//		std::ofstream out;
    		if (!open_file(argv[i], *out, append))
    			return 1;
 		streams.push_back(out);
-//		std::cout << sizeof(*bufs[0]) << std::endl;
-//		teebuf::ptr p = (*out).rdbuf();
-//		bufs.push_back(p);
-
-   	}
-//	bufs.push_back(std::cout.rdbuf());
-//	std::shared_ptr<std::ofstream> out(new std::ofstream);
-//   	redirecter r(*out, std::cout );
-//	streams.push_back(std::move(std::cout));
-//	streams.push_back(out);
-
+	   }
     teestream tee(streams);
-//	teestream tee(std::cout, log);
 	tee << std::cin.rdbuf();
-//	r.a & 1;
-//	sleep(100);
     return 0;
 }
--- tee.hpp
+++ tee.hpp
@@ -8,10 +8,7 @@ class basic_teebuf : public std::basic_streambuf<char_type, traits>
     typedef typename traits::int_type int_type;
 	typedef typename std::vector< std::basic_streambuf<char_type, traits> * > ptrs;
 	typedef typename std::basic_streambuf<char_type, traits> * ptr;
-//	typedef typename std::vector< std::basic_streambuf<char_type, traits> * >::iterator buf_it;
 	typedef typename ptrs::iterator buf_it;
-
-//	basic_teebuf(ptrs & _bufs) : bufs(std::move(_bufs)) {}
 
 	basic_teebuf(std::vector<std::shared_ptr<std::ofstream> > & streams) {
 		typedef typename std::vector<std::shared_ptr<std::ofstream> >::iterator stream_it;
@@ -19,23 +16,11 @@ class basic_teebuf : public std::basic_streambuf<char_type, traits>
 			bufs.push_back((*it)->rdbuf());
 		bufs.push_back(std::cout.rdbuf());  // this is hardcoded solution
 	}
-
-//	basic_teebuf(std::basic_streambuf<char_type, traits> * sb1,
-//                 std::basic_streambuf<char_type, traits> * sb2)
-//      : sb1(sb1)
-//      , sb2(sb2) {}
-
- private:    
-//    virtual int sync() {
-//        int const r1 = sb1->pubsync();
-//        int const r2 = sb2->pubsync();
-//        return r1 == 0 && r2 == 0 ? 0 : -1;
-//    }
+ private:
 
 	virtual int sync() {
 		bool flag = false;
 
-//		std::cout << "size: " << bufs.size() << std::endl;
 		for (buf_it it = bufs.begin(); it != bufs.end(); it++) {
 			if ( (*it)->pubsync() == 0 )
 				flag = true;
@@ -53,7 +38,6 @@ class basic_teebuf : public std::basic_streambuf<char_type, traits>
 
 		char_type const ch = traits::to_char_type(c);
 		for (buf_it it = bufs.begin(); it != bufs.end(); it++) {
-//			std::cout << "d" << sizeof(**it) << std::endl;
 			int_type const r = (*it)->sputc(ch);
 			if ( traits::eq_int_type(r, eof) )
 				flag = true;
@@ -62,39 +46,18 @@ class basic_teebuf : public std::basic_streambuf<char_type, traits>
     }
 
  private:
-//	std::vector< std::basic_streambuf<char_type, traits> * > bufs;
 	ptrs bufs;
 
-//    std::basic_streambuf<char_type, traits> * sb1;
-//    std::basic_streambuf<char_type, traits> * sb2;
 };
 
 typedef basic_teebuf<char> teebuf;
 
 class teestream : public std::ostream {
  public:
-//	teestream(teebuf::ptrs & bufs)
-//	teestream(std::vector< std::basic_streambuf<char> * > & bufs)
-//			: std::ostream(&tbuf)
-//			, tbuf(bufs) {}
-//	teestream(std::vector< std::basic_streambuf<char> * > & bufs)
 
 	teestream(std::vector<std::shared_ptr<std::ofstream> > & streams)
 			: std::ostream(&tbuf)
 			, tbuf(streams) {}
  private:
 	teebuf tbuf;
-};
-
-
-class redirecter
-{
- public:
-	redirecter(std::ofstream & dst, std::ostream & src)
-			: src(src)
-			, sbuf(src.rdbuf(dst.rdbuf())) {}
-	~redirecter() { src.rdbuf(sbuf); }
- private:
-	std::ostream & src;
-	std::streambuf * const sbuf;
 };
